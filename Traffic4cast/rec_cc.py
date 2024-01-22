daf58f4b6f21062bcf5e1eefc3b5c24a44310243
Pavlo Pyvovar <pavlo.pyvovar@ut.ee>
2024-01-22 16:58:16 +0200

    Add A3T-GCN model experiment

--- Traffic4cast/rec_cc.py
+++ Traffic4cast/rec_cc.py
@@ -1,15 +1,14 @@
 import os
 import argparse
 
-import statistics
-from collections import defaultdict
-
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
 import torch
 import torch.nn.functional as F
 import torch_geometric
+from torch_geometric.nn import GATv2Conv as GCNConv
+from torch_geometric_temporal.nn.recurrent import A3TGCN
 import tqdm
 from torch import nn
 from pathlib import Path
@@ -46,7 +45,6 @@ parser.add_argument('--num_layers', type=int, default=3,
 parser.add_argument('--batch_size', type=int, default=2, help="batch_size")
 parser.add_argument('--epochs', type=int, default=20, help="epochs")
 
-parser.add_argument('--dropout', type=float, default=0.0, help="dropout")
 # if enriched = True make sure that edge_attributes specified correctly
 parser.add_argument('--enriched', type=bool, default=True,
                     help="whether to use enriched road edges or not")
@@ -81,8 +79,7 @@ opt[
 
 
 class RecLinear(nn.Module):
-    def __init__(self, num_edges, num_nodes, num_attrs, in_channels, hidden_channels, out_channels, num_layers,
-                 dropout):
+    def __init__(self, num_edges, num_nodes, num_attrs, in_channels, hidden_channels, out_channels, num_layers):
 
         super(RecLinear, self).__init__()
 
@@ -223,6 +220,150 @@ class RecLinear(nn.Module):
 
         xf = torch.cat([embed, self.attr_lin1(attr), x,
                        x1, time_embed, week_embed], dim=1)
+
+        for lin in self.lins[:-1]:
+            xf = lin(xf)
+            xf = self.gelu(xf)
+
+        xf = self.lins[-1](xf)
+
+        return xf, x_rec
+
+
+class Traffic4castA3TGCN(nn.Module):
+    def __init__(self, num_edges, num_nodes, num_attrs, in_channels, hidden_channels, out_channels, num_layers, periods):
+
+        super(Traffic4castA3TGCN, self).__init__()
+
+        self.embed = nn.Embedding(num_edges, hidden_channels)
+        self.node_embed = nn.Embedding(num_nodes, hidden_channels)
+        self.node_embed1 = nn.Embedding(num_nodes, 4)
+        self.time_embed = nn.Embedding(96, hidden_channels)
+        self.week_embed = nn.Embedding(7, hidden_channels)
+        self.node_index = torch.arange(0, num_nodes).to(device)
+
+        self.node_lin = nn.Linear(in_channels, hidden_channels)
+        self.node_lin1 = nn.Linear(hidden_channels * 2, hidden_channels)
+        self.attr_lin = nn.Linear(num_attrs, hidden_channels)
+        self.attr_lin1 = nn.Sequential(nn.Linear(num_attrs, hidden_channels), nn.LeakyReLU(),
+                                       nn.Linear(hidden_channels, hidden_channels))
+        self.lins = torch.nn.ModuleList()
+        self.lins.append(torch.nn.Linear(hidden_channels * 6, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.lins.append(torch.nn.Linear(hidden_channels, hidden_channels))
+        self.lins.append(torch.nn.Linear(hidden_channels, out_channels))
+
+        self.fc1 = nn.Linear(num_nodes, 256)
+        self.fc2 = nn.Linear(256, 32)  # mean vector
+        self.fc3 = nn.Linear(256, 32)  # standard deviation vector
+        self.fc4 = nn.Linear(32, 256)
+        self.fc5 = nn.Linear(256, num_nodes)
+
+        self.tgnn = A3TGCN(in_channels=in_channels, out_channels=hidden_channels, periods=periods)
+
+        self.conv1 = torch.nn.ModuleList()
+        for i in range(3):
+            self.conv1.append(GCNConv(hidden_channels, hidden_channels, edge_dim=hidden_channels))
+
+        self.conv2 = torch.nn.ModuleList()
+        for i in range(3):
+            self.conv2.append(GCNConv(hidden_channels, hidden_channels, edge_dim=hidden_channels))
+
+        self.gcn_lin1 = nn.Linear(in_channels * 2, in_channels)
+        self.gcn_lin2 = nn.Linear(hidden_channels * 2, hidden_channels)
+
+    def gelu(self, x):
+        return 0.5 * x * (1 + F.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
+    def encode(self, x):
+        h = self.gelu(self.fc1(x))
+        return self.fc2(h), self.fc3(h)
+
+    def reparameterize(self, mu, log_var):
+        """Gaussian sampling"""
+        std = torch.exp(log_var / 2)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def decode(self, z):
+        h = self.gelu(self.fc4(z))
+        h = self.fc5(h)
+        return h
+
+    def reset_parameters(self):
+        self.embed.reset_parameters()
+        self.node_embed.reset_parameters()
+        self.node_embed1.reset_parameters()
+        self.time_embed.reset_parameters()
+        self.node_lin.reset_parameters()
+        self.node_lin1.reset_parameters()
+        self.attr_lin.reset_parameters()
+        self.fc1.reset_parameters()
+        self.fc2.reset_parameters()
+        self.fc3.reset_parameters()
+        self.fc4.reset_parameters()
+        self.fc5.reset_parameters()
+        for lin in self.lins:
+            lin.reset_parameters()
+        for lin in self.conv1:
+            lin.reset_parameters()
+        for lin in self.conv2:
+            lin.reset_parameters()
+        self.gcn_lin1.reset_parameters()
+        self.gcn_lin2.reset_parameters()
+
+    def forward(self, index, edge_index, x, attr, cur_t, cur_w):
+        mask_idx = (torch.sum(x, dim=1, keepdim=True) != nan_to_num_map[opt["city"]] * 4).type(torch.float)
+
+        xmax = 23.91
+        xmin = nan_to_num_map[opt["city"]]
+        x_norm = (x - xmin) / (xmax - xmin)
+
+        ratio = 0.8 + 0.4 * np.random.rand(1)[0]
+
+        x_norm = x_norm * ratio
+
+        drop_idx = (torch.rand_like(x_norm[:, 0:1]) > 0.4).type(torch.float)
+        x_norm = x_norm * drop_idx
+
+        x_norm = torch.transpose(x_norm, 0, 1)
+        mu, log_var = self.encode(x_norm)
+        z = self.reparameterize(mu, log_var)
+        x_rec = self.decode(z)
+
+        x_rec = x_rec / ratio
+
+        x_rec = torch.transpose(x_rec, 0, 1)
+        x_rec = x_rec * (xmax - xmin) + xmin
+        x_rec1 = mask_idx * x + (1 - mask_idx) * x_rec
+
+        attr1 = self.attr_lin(attr)
+        embed = self.embed(index)
+
+        node_embed = self.node_embed(self.node_index)
+        pre_data = node_embed
+        for conv in self.conv1:
+            node_embed = conv(node_embed, edge_index, attr1)
+            node_embed = self.gelu(node_embed) + pre_data
+
+        data = x_rec1
+        x_i = torch.index_select(data, 0, edge_index[0])
+        x_j = torch.index_select(data, 0, edge_index[1])
+        x = torch.concat([x_i, x_j], dim=1)
+        x = self.gcn_lin1(x)
+
+        # Instead of just GCN apply A3TGCN
+        x = self.tgnn(x.unsqueeze(2), edge_index).relu()
+
+        x_i = torch.index_select(node_embed, 0, edge_index[0])
+        x_j = torch.index_select(node_embed, 0, edge_index[1])
+        x1 = torch.concat([x_i, x_j], dim=1)
+        x1 = self.gcn_lin2(x1)
+
+        time_embed = self.time_embed(cur_t.long())
+        week_embed = self.week_embed(cur_w.long())
+
+        xf = torch.cat([embed, self.attr_lin1(attr), x, x1, time_embed, week_embed], dim=1)
 
         for lin in self.lins[:-1]:
             xf = lin(xf)
